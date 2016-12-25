d4f07a67226dfc20d6a162c4a0ab50b45a1e4bfc
Manoj Kumar <manoj.k@fetchr.us>
2016-12-25 16:18:27 +0400

    Initial commit

---
+++ test_kinesis.py
@@ -0,0 +1,30 @@
+from fetchr_kinesis import push_to_erp_stream
+from fetchr_erpstream_utils import NotAJSONError
+from moto import mock_kinesis
+
+import pytest
+import sure
+import boto3
+
+@mock_kinesis
+def test_invalid_payload_key():
+    is_success, message = push_to_erp_stream("Sample", "some-access-key", "secret-access-key", 1234, "1234")
+    assert is_success == False
+
+@mock_kinesis
+def test_invalid_payload():
+    with pytest.raises(NotAJSONError):
+        push_to_erp_stream("Sample", "some-access-key", "secret-access-key", "1234", set([1,2,3]))
+
+@mock_kinesis
+def test_invalid_streamname():
+    is_success, message = push_to_erp_stream("sample", "some-access-key", "secret-access-key", "1234", {"a":123,"b":456})
+    assert is_success == False
+
+
+@mock_kinesis
+def test_valid_streampush():
+    _kinesis = boto3.client("kinesis", region_name="ap-southeast-1")
+    _kinesis.create_stream(StreamName="sample-stream", ShardCount=2)
+    is_success, message = push_to_erp_stream("sample-stream", "some-access-key", "secret-access-key", "1234", {"a":123,"b":456})
+    assert is_success == True
---
+++ test_utils.py
@@ -0,0 +1,24 @@
+import pytest
+from fetchr_erpstream_utils import as_json_of, NotAJSONError
+
+def test_valid_string_json_payload():
+    _payload = "addf"
+    _json_payload = as_json_of(_payload)
+    assert '\"'+_payload+'\"' == _json_payload
+
+def test_valid_dict_payload():
+    _payload = {'a': '123', 'b':123}
+    _json_payload = as_json_of(_payload)
+    assert isinstance(_json_payload, basestring)
+
+
+def test_valid_int_payload():
+    _payload = 123
+    _json_payload = as_json_of(123)
+    assert isinstance(_json_payload, basestring)
+
+
+def test_invalid_json_object():
+    _payload = set([1,2])
+    with pytest.raises(NotAJSONError):
+        as_json_of(_payload)
